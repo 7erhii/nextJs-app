c77b958b032ebbdf39cc36a5e5a7d9255c203aeb
7erhiy <7erhiy@gmail.com>
2023-12-24 17:00:19 -0400

    adding page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,65 @@
-import Image from 'next/image'
-import React, { useState } from 'react';
-
+import Image from "next/image";
+import React, { useState } from "react";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black-100 flex items-center justify-center">
-      <div className="bg-gray-400 shadow-lg rounded-lg p-8 max-w-md">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">
-          Добро пожаловать!
-        </h1>
-        <p className="text-gray-800 mb-6">
-          Страница для демостации
-        </p>
-        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
-          Click Here!
-        </button>
-      </div>
-    </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <Head>
+        <title>Next.js with Tailwind CSS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{" "}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Get started by editing{" "}
+          <code className="p-3 font-mono text-lg bg-gray-200 rounded-md">
+            pages/index.js
+          </code>{" "}
+          {/* This line was corrected */}
+        </p>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <a
+            href="https://nextjs.org/docs"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
+
+          <a
+            href="https://tailwindcss.com"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Learn Tailwind &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Learn how to integrate Tailwind CSS in your projects.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
+    </div>
+  );
+}
